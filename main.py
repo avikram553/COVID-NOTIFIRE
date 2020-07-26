ff7ff999452b326a8b5d96c19aec3616a1dcbe94
avikram553 <avikram553Agmail.com>
2020-07-26 12:05:59 -0700

    Telanganna was not icluded

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def getCovidData(url):
 	#print(soup.prettify)
 	mydataString=''
 	for tr in soup.find('tbody').find_all('tr'):
-		if '*' in tr.text or '#' in tr.text:
+		if tr.text[1]=='*' or '#' in tr.text:
 			continue
 		else:
 			mydataString+=tr.text
@@ -28,18 +28,35 @@ def getCovidData(url):
 
 	data[0].pop(0)
 	data[len(data)-1].pop(-1)
+	Total=0
+	TotalCured=0
+	TotalDeaths=0
+	for i in range(len(data)):
+		#print(data[i])
+		Total+=int(data[i][5])
+		TotalCured+=int(data[i][3])
+		TotalDeaths+=int(data[i][4])
+	#print(Total)
+	#print(TotalDeaths)
+	notiTitle='Cases of COVID 19'
+	notiText=f"Total:	{Total}\nCured:	{TotalCured}\nDeaths:	{TotalDeaths}"
+	notifyme(notiTitle,notiText)
+	time.sleep(5)
+	#print(*data)
 	states=['Uttar Pradesh','Delhi','Maharashtra']
 	for i in range(len(data)):
 		if data[i][1] in states:
 			endProcess=False
-			print(data[i])
+			#print(data[i])
 			notiTitle='Cases of COVID 19'
-			notiText=f"STATE:	{data[i][1]}\nTotal:	{int(data[i][2])+int(data[i][3])}\nCured:	{data[i][4]}\nDeaths:	{data[i][5]}"
+			notiText=f"STATE:	{data[i][1]}\nTotal:	{int(data[i][5])}\nCured:	{data[i][3]}\nDeaths:	{data[i][4]}"
 			notifyme(notiTitle,notiText)
 			'''while not endProcess:
 				pass'''
 			time.sleep(5)
 if __name__ == '__main__':
 	#notifyme("COVID 19 ALERT","Help to spread awareness")
+	#while True:
 	getCovidData("https://www.mohfw.gov.in/")
 	endProcess=False
+		#time.sleep(3)
